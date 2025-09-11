cab35d98f63b19fd637bcf9cd5218639e014f5d9
Revati <revati1701@gmail.com>
2025-09-10 20:24:47 -0700

    use path variable for delete by id

--- src/main/java/com/mahajan/habittracker/controller/HabitController.java
+++ src/main/java/com/mahajan/habittracker/controller/HabitController.java
@@ -3,6 +3,7 @@ package com.mahajan.habittracker.controller;
 import com.mahajan.habittracker.model.Habit;
 import com.mahajan.habittracker.repository.HabitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,8 +25,9 @@ public class HabitController {
         return habitRepository.save(habit);
     }
 
-    @DeleteMapping
-    public void deleteById(@RequestParam Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         habitRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
