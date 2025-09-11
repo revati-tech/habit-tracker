2a34568f5196ebcd22c594e7c123649c0bf04175
Revati <revati1701@gmail.com>
2025-09-10 20:03:40 -0700

    Added annotation Generated Identity Habit model

--- src/main/java/com/mahajan/habittracker/model/Habit.java
+++ src/main/java/com/mahajan/habittracker/model/Habit.java
@@ -1,11 +1,14 @@
 package com.mahajan.habittracker.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Habit {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
